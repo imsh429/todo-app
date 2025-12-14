ec41acddd57f6b09fbbedbf3dc3e3ca1cea7eb22
imsh429 <sh99429@naver.com>
2025-12-14 21:46:08 +0900

    main.js 수정

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// src/main.js
 import './assets/main.css'
 
 import { createApp } from 'vue'
@@ -6,9 +7,16 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+// PrimeVue 추가
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/lara-light-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue)  // PrimeVue 사용
 
 app.mount('#app')
