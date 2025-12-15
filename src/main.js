47d650f18e2e575bf9ae3ef55ab09c7253f3eed1
imsh429 <sh99429@naver.com>
2025-12-15 22:33:27 +0900

    env 추가

--- src/main.js
+++ src/main.js
@@ -1,14 +1,12 @@
 // src/main.js
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import PrimeVue from 'primevue/config'
 
 import App from './App.vue'
 import router from './router'
 
-// PrimeVue 추가
-import PrimeVue from 'primevue/config'
+// PrimeVue CSS
 import 'primevue/resources/themes/lara-light-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
@@ -17,6 +15,6 @@ const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-app.use(PrimeVue)  // PrimeVue 사용
+app.use(PrimeVue)
 
 app.mount('#app')
