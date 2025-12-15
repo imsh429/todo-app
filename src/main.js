938656a439d4cd23baa94cc7e7e15b3fa4e36b56
imsh429 <sh99429@naver.com>
2025-12-15 22:47:52 +0900

    기본 설정

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
-
 import App from './App.vue'
 import router from './router'
 
@@ -10,6 +9,9 @@ import router from './router'
 import 'primevue/resources/themes/lara-light-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
 import 'primeicons/primeicons.css'
+
+// 커스텀 CSS
+import './assets/main.css'
 
 const app = createApp(App)
 
